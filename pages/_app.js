6ff4f9154dff5cb5796ade7befdfcd8d3d982d8a
jstnknr <143372221+jstnknr@users.noreply.github.com>
2025-08-02 17:24:09 -0400

    Add Title

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import Header from '../components/Header';
 import { CourseProvider } from '../context/CourseContext';
 
 export default function App({ Component, pageProps }) {
   return (
-    <CourseProvider>
-      <Header />
-      <Component {...pageProps} />
-    </CourseProvider>
+    <>
+      <Head>
+        {/* default title */}
+        <title>PLNR</title>
+      </Head>
+
+      <CourseProvider>
+        <Header />
+        <Component {...pageProps} />
+      </CourseProvider>
+    </>
   );
 }
